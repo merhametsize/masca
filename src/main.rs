39c02f30ef8405d1fc0a4ea574ef3cf5bd275b1f
Gabriele Cassetta <gabriele.cassetta98@gmail.com>
2026-03-10 16:33:54 +0100

    Added a new test FEN.

--- src/main.rs
+++ src/main.rs
@@ -35,14 +35,16 @@ const QUIET_SACRIFICE: &str = "r2q1rk1/pp3ppp/2nb1n2/3pp3/3P4/2PBPN2/PP3PPP/RNBQ
 const ZUGZWANG: &str = "8/8/8/3k4/3P4/3K4/8/8 w - - 0 1";
 #[allow(dead_code)]
 const TACTICAL_MATE: &str = "r1b1k2r/pppp1ppp/2n2n2/1B2p3/4P3/2N5/PPPP1PPP/R1BQK2R w kq - 0 1";
+#[allow(dead_code)]
+const BOH: &str = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
 
 fn main() {
     //benchmark_perft(6);
     let mut board = Board::new();
-    board.from_fen(KIWIPETE).unwrap();
+    board.from_fen(BOH).unwrap();
     //board.set_startpos();
     board.print();
 
     let mut searcher = Searcher::new(&mut board);
-    searcher.iterative_deepening(11);
+    searcher.iterative_deepening(15);
 }
